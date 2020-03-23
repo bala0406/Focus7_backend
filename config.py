0d67d4c034bdb2c5eefb9c53732803a2b1bdcb96
Bala <balaganesh12.12@outlook.com>
2020-03-23 12:45:23 +0530

    created postgres db on heroku

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "postgres://mtvjyykijbejqz:52df3302a3c23d8f444fbce42996988421315657eb3ec8fbe1b99bf7328bc9a5@ec2-52-207-93-32.compute-1.amazonaws.com:5432/degckucv04ef9e"
 
 class StagingConfig(Config):
     DEVELOPMENT = True
