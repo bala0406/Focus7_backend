6adce932dbb974ac3af73f59a9691e525b06ede5
Bala <balaganesh12.12@outlook.com>
2020-03-24 18:55:31 +0530

    handled exception in returing api without data

--- api.py
+++ api.py
@@ -12,30 +12,33 @@ def getQuestions():
 
     questionsList = []
     rowCount = len(Question.query.all())
-    # questionDict = {}
-    for i in range(7):
 
-        question = Question.query.get(randint(1, rowCount))
-        chapter_name = question.chapter_name
-        quest_ion = question.question
-        option_1 = question.option_1
-        option_2 = question.option_2
-        option_2 = question.option_2
-        option_3 = question.option_3
-        option_4 = question.option_4
-        correct_option = question.correct_option
-        question_mark = question.question_mark
-        hint = question.hint if question.hint is not None else ""
-        questionDict = {
-            "chapter_name": chapter_name,
-            "question": quest_ion,
-            "option_1": option_1,
-            "option_2": option_2,
-            "option_3": option_3,
-            "option_4": option_4,
-            "correct_option": correct_option,
-            "question_mark": question_mark,
-            "hint": hint
-        }
-        questionsList.append(questionDict)
-    return jsonify(questionsList)
+    if rowCount != 0:
+        for i in range(7):
+
+            question = Question.query.get(randint(1, rowCount))
+            chapter_name = question.chapter_name
+            quest_ion = question.question
+            option_1 = question.option_1
+            option_2 = question.option_2
+            option_2 = question.option_2
+            option_3 = question.option_3
+            option_4 = question.option_4
+            correct_option = question.correct_option
+            question_mark = question.question_mark
+            hint = question.hint if question.hint is not None else ""
+            questionDict = {
+                "chapter_name": chapter_name,
+                "question": quest_ion,
+                "option_1": option_1,
+                "option_2": option_2,
+                "option_3": option_3,
+                "option_4": option_4,
+                "correct_option": correct_option,
+                "question_mark": question_mark,
+                "hint": hint
+            }
+            questionsList.append(questionDict)
+        return jsonify(questionsList)
+    else:
+        return "No data available"    
